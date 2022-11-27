96c550c2e2fa15b02ed91fae546a4ee33777e7ca
ashtiv <ashishtiwari05032002@gmail.com>
2022-11-28 00:30:16 +0530

    Add all my files

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const cors = require("cors");
+require('dotenv').config();
 const stripe = require("stripe")(
-    "your stripe id"
+    process.env.stripe_id
 );
 
 // API
